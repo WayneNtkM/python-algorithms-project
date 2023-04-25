b0bc1fcf46558a93453bf380af0df9535ee9d6ce
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-25 16:51:02 -0300

    feat: is_anagram implemented

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,31 @@
+def merge_sort(word):
+    if len(word) < 2:
+        return word
+    result = []
+    mid = int(len(word) / 2)
+    y = merge_sort(word[:mid])
+    z = merge_sort(word[mid:])
+    i = 0
+    j = 0
+    while i < len(y) and j < len(z):
+        if y[i] > z[j]:
+            result.append(z[j])
+            j += 1
+        else:
+            result.append(y[i])
+            i += 1
+    result += y[i:]
+    result += z[j:]
+    return "".join(result)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(first_string) == 0 and len(second_string) == 0:
+        return (first_string, second_string, False)
+
+    sorted_first = merge_sort(first_string.lower())
+    sorted_second = merge_sort(second_string.lower())
+
+    anagram = sorted_first == sorted_second
+
+    return (sorted_first, sorted_second, anagram)
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -9,5 +9,5 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
 
     if low_index < high_index + 1:
-            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return True
