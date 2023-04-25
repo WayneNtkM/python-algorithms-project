d3da61051087a85a272592624757d0e152fa689d
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-25 17:28:57 -0300

    feat: find_duplicate implemented

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,19 @@
+from collections import Counter
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) < 2:
+        return False
+
+    if not all(isinstance(n, int) for n in nums):
+        return False
+
+    if any(n < 0 for n in nums):
+        return False
+
+    counter = Counter(nums)
+
+    if all(d == 1 for d in counter.values()):
+        return False
+
+    return counter.most_common(1)[0][0]
