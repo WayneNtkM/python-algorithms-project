96fb977b2ddc758315d201c16f4184e9720529a1
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-25 14:56:34 -0300

    test: test_encrypt implemented

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,20 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+
+mock = [
+    ('Teste', 2, "ets_eT"),
+    ('Teste', 3, "seT_et"),
+    ('Teste', 9, "etseT"),
+]
 
 
 def test_encrypt_message():
-    pass
+    for m, k, r in mock:
+        result = encrypt_message(m , k)
+        assert r == result
+
+    with pytest.raises(
+        TypeError, match="tipo inválido para key"
+    ):
+        encrypt_message(2, 'dois')
