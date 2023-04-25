6e4236bbde169eda402f50fbb2d4507bc7040002
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-25 17:13:27 -0300

    feat: is_palindrome_iterative implemented

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    if word == word[::-1]:
+        return True
+    else:
+        return False
